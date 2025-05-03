b585d6883c5a9a7445af1eaac0da4b7820532bc8
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-03 15:06:16 +0330

    Update WelcomeActivity.java

--- oblivion-main/app/src/main/java/com/sq/oblivionvpn/WelcomeActivity.java
+++ oblivion-main/app/src/main/java/com/sq/oblivionvpn/WelcomeActivity.java
@@ -1,13 +1,19 @@
 package com.sq.oblivionvpn;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Handler;
 
 public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(WelcomeActivity.this, EndpointActivity.class));
+            finish();
+        }, 3000); // 3 ثانیه تأخیر برای نمایش لوگو و متن
     }
 }
