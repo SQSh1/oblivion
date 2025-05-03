fd0b96f37bfdd5ce271569dbb8cb4ff6373b969c
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-03 15:10:25 +0330

    Create EndpointActivity.java

---
+++ oblivion-main/app/src/main/java/com/sq/oblivionvpn/EndpointActivity.java
@@ -0,0 +1,12 @@
+package com.sq.oblivionvpn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EndpointActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endpoint);
+    }
+}
